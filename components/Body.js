c60eaee0fc8688d7b16c00d3a3ffc91709259b9c
PauloRFOliveira <paulo.oliveira.701@ufrn.edu.br>
2022-10-08 22:35:26 -0300

    Feature: Configuração básica dos elementos da tela inicial.

--- components/Body.js
+++ components/Body.js
@@ -1,12 +1,19 @@
 import React from "react";
-import {StyleSheet} from "react-native";
+import {StyleSheet, View} from "react-native";
 
 export default function Body(){
     return(
+        <View style={styles.container}>
 
+
+
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: "#fff"
+    },
 });
